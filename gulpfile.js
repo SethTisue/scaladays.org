7ac93d9cf933eb8ea364931ec4aca2472c8f59fa
Maxime DANTEC <hello@warry.fr>
2014-02-28 13:37:26 +0100

    almost done

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
 
-
 // Compile stylus files
 gulp.task('stylus', function () {
   return gulp.src('assets/styl/main.styl')
-      .pipe(stylus({use: ['nib']}))
+      .pipe(stylus({use: ['nib'], set:['compress']}))
       .pipe(gulp.dest('assets/css'))
 })
-
 
 // Default gulp task to run
 gulp.task('default', function(){
